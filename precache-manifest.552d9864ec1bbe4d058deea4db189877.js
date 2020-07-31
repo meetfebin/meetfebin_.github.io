25dbbf76a1cfb84d40e40d6907577dc3e3fd9ea1
Febin John James <jamesfebin@gmail.com>
2020-07-31 18:00:41 +0530

    Build

---
+++ precache-manifest.552d9864ec1bbe4d058deea4db189877.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752b619791f1e1774efb3ffa2fe18de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16159309707276657b71",
+    "url": "/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "3d45ae94af09e315d88d",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "f8344efc88c17b3bcfee",
+    "url": "/static/css/main.bfd7dc14.chunk.css"
+  },
+  {
+    "revision": "17edf5dbab7ee0a8558d",
+    "url": "/static/js/2.10a85e41.chunk.js"
+  },
+  {
+    "revision": "16159309707276657b71",
+    "url": "/static/js/3.9366f722.chunk.js"
+  },
+  {
+    "revision": "3d45ae94af09e315d88d",
+    "url": "/static/js/4.96d0cec7.chunk.js"
+  },
+  {
+    "revision": "f8344efc88c17b3bcfee",
+    "url": "/static/js/main.d9854bef.chunk.js"
+  },
+  {
+    "revision": "92f303d704569ec862c7",
+    "url": "/static/js/runtime-main.9af7afe7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "9cbed9824e5d41b4d5b88f5415f3038e",
+    "url": "/static/media/books.9cbed982.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e21c745da5b96865ee9bd53104aa9723",
+    "url": "/static/media/febin1.e21c745d.jpg"
+  },
+  {
+    "revision": "d6187e9e9152ce651b9d001f3dec077f",
+    "url": "/static/media/febin2.d6187e9e.jpg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3f01de56ecd17e7c5e16f2269dfb06e7",
+    "url": "/static/media/looking.3f01de56.jpg"
+  },
+  {
+    "revision": "a20638e74087cd8867831ec1ea4628e8",
+    "url": "/static/media/meetfebin.a20638e7.jpg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
